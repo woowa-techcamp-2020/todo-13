478ea99d88ed348c07e180c886d51c626cf6d589
sss5793 <>
2020-07-15 16:38:16 +0900

    Add drag and drop experiment
    - HTML drag and drop API 를 사용해 column, card 컴포넌트의 drag and drop 구현

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -14,11 +14,48 @@ export default class Column {
   paintCard(column) {
     const contents = ["설정파일 분리 리팩토링 작업", "데모 환경 구성"];
 
-    contents.forEach((content) => new Card(column, { content }));
+    contents.forEach(
+      (content) => new Card(column, { category: this.title, content })
+    );
   }
 
   render() {
     const column = makeElementWithClass("div", "column");
+
+    function getDragAfterElement(container, y) {
+      const cards = [
+        ...container.querySelectorAll(".draggable:not(.dragging)"),
+      ];
+      return cards.reduce(
+        (closeset, child) => {
+          const boundingBox = child.getBoundingClientRect();
+          const offset = y - boundingBox.top - boundingBox.height / 2;
+
+          if (offset < 0 && offset > closeset.offset) {
+            return {
+              offset: offset,
+              element: child,
+            };
+          } else {
+            return closeset;
+          }
+        },
+        { offset: Number.NEGATIVE_INFINITY }
+      ).element;
+    }
+
+    column.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(column, e.clientY);
+      const draggingElement = document.querySelector(".dragging");
+
+      if (afterElement === null) {
+        column.appendChild(draggingElement);
+      } else {
+        column.insertBefore(draggingElement, afterElement);
+      }
+    });
+
     const title = makeElementWithClass("div", "column_title");
     title.innerHTML = this.title;
     title.addEventListener("dblclick", () => showPopup(MSG.DELETE));
