88f8c073fec18c9a1c8bca618f843443fd307186
Pigrabbit <donghyuk1003@gmail.com>
2020-07-15 15:30:27 +0900

    Setup server
    - Docker를 이용해 mysql 서버를 로컬에서 실행합니다.
    - mysql 드라이버를 이용해 db 와 연동합니다.
    - dotenv 를 이용해 중요정보를 숨깁니다.

---
+++ server/app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const mysql = require("mysql");
+
+console.log(process.env.MYSQL_DATABASE);
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.MYSQL_DATABASE,
+  port: 3306,
+});
+
+connection.connect((e) => {
+  if (e) {
+    console.error(e);
+    return process.exit(22);
+  }
+  console.log("DB connection established!");
+});
+
+app.get("/", (req, res) => res.send("Hello World! Docker setup done~~~"));
+
+app.get("/card", (req, res) => {
+  connection.query("SELECT * FROM todo.Cards", (err, rows, fields) => {
+    if (err) throw err;
+    console.log("card info is:", rows);
+    res.send(rows);
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
---
+++ server/sql_scripts/insertCard.sql
@@ -0,0 +1,2 @@
+INSERT INTO Cards(id, author, last_updated, content, category)
+VALUES(null, 'user1', now(), 'first message', '할 일');
