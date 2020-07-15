07fbfba295f76f36d3c7cd2c89c0c8fd5317e6cd
Pigrabbit <donghyuk1003@gmail.com>
2020-07-15 15:33:53 +0900

    Fix typo
    - 환경변수 관련 오타 수정

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.MYSQL_DATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: 3306,
 });
 
