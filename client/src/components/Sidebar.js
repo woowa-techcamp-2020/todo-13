27c2df7e49f952fd9184ba4c7123876fe657b418
sss5793 <>
2020-07-16 16:45:01 +0900

    Add Sidebar style and animation
    - Sidebar 스타일 추가 및 애니메이션 기능  구현
    - Sidebar, Item scss 파일 추가
    - Modal, Popup z-index 설정
    - 아이콘 사용을 위해 script 추가

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,5 +1,48 @@
+import "./Sidebar.scss";
+import { makeElementWithClass } from "../utils/util";
+
 export default class Sidebar {
-    constructor($target) {
-        this.$target = $target;
-    }
+  constructor($target, props) {
+    this.$target = $target;
+
+    this.render();
+  }
+
+  render() {
+    const sidebar = makeElementWithClass({
+      elementType: "div",
+      className: "sidebar",
+    });
+    const sidebarHeader = makeElementWithClass({
+      elementType: "div",
+      className: "sidebar-header",
+    });
+    const title = makeElementWithClass({
+      elementType: "div",
+      className: "sidebar-header-title",
+      content: "Menu",
+    });
+    const close = makeElementWithClass({
+      elementType: "div",
+      className: "sidebar-header-close",
+      content: "<ion-icon name='close-outline'></ion-icon>",
+    });
+    const sidebarContents = makeElementWithClass({
+      elementType: "div",
+      className: "sidebar-contents",
+    });
+
+    close.addEventListener("click", () => {
+      sidebar.classList.remove("active");
+      sidebar.classList.add("out");
+    });
+
+    sidebarHeader.appendChild(title);
+    sidebarHeader.appendChild(close);
+
+    sidebar.appendChild(sidebarHeader);
+    sidebar.appendChild(sidebarContents);
+
+    this.$target.appendChild(sidebar);
+  }
 }
