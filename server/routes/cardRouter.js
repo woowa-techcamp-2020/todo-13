6650c13747668a6b3d830696db651a6e296cd006
sss5793 <>
2020-07-17 11:33:19 +0900

    Add remove card api

--- server/routes/cardRouter.js
+++ server/routes/cardRouter.js
@@ -4,6 +4,7 @@ const cardController = require("../controllers/CardController");
 
 router.get("/", cardController.getAllCards);
 router.get("/:id", cardController.getOneCard);
+router.delete("/:id", cardController.deleteOneCard);
 router.post("/", cardController.createCard);
 
 // TODO: PUT, PATCH, DELETE 메소드 api 구현하기
