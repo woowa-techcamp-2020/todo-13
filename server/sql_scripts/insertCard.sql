f738353d41e2afbe9f7c7d26ca00c889d42f7e3c
Pigrabbit <donghyuk1003@gmail.com>
2020-07-20 17:53:26 +0900

    Add sql script to create User and Activity tables

--- server/sql_scripts/insertCard.sql
+++ server/sql_scripts/insertCard.sql
@@ -1,2 +1,5 @@
-INSERT INTO Cards(id, author, last_updated, content, category)
-VALUES(null, 'user1', '2020-07-01 08:30:02', '✏docker 공부하기', '해야할 일');
+INSERT INTO Users(id, username, authority)
+VALUES(null, "user1", "admin");
+
+INSERT INTO Cards(id, userid, last_updated, content, category)
+VALUES(null, 1, '2020-07-01 08:30:02', '✏docker 공부하기', '해야할 일');
