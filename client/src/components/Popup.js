b2fb06cfe10a88eed13e9510bf7ab82c8a719475
Pigrabbit <donghyuk1003@gmail.com>
2020-07-21 01:31:20 +0900

    Remove unused comment

--- client/src/components/Popup.js
+++ client/src/components/Popup.js
@@ -1,5 +1,4 @@
 import {
-  makeElementWithClass,
   bindEvent
 } from "../utils/util";
 import "./Popup.scss";
@@ -53,66 +52,10 @@ export default function Popup() {
     bindEvent(".popup-confirm", "click", onConfirmBtnClick);
   }
 
-  subscribe(componentName, 'popupMessage', render);
+  // subscribe(componentName, 'popupMessage', render);
   subscribe(componentName, 'isPopupVisible', render);
 
   setTimeout(render, 0);
 
   return `<div class=popup-wrapper></div>`;
 }
-
-// export default class Popup {
-//   constructor($target, props) {
-//     this.$target = $target;
-
-//     this.render();
-//   }
-
-//   setState(data) {
-//     this.data = data;
-//     this.render();
-//   }
-
-//   render() {
-//     const popup = makeElementWithClass({
-//       elementType: "div",
-//       className: "popup",
-//     });
-//     const popupBox = makeElementWithClass({
-//       elementType: "div",
-//       className: "popup-box",
-//     });
-//     const message = makeElementWithClass({
-//       elementType: "div",
-//       className: "popup-message",
-//     });
-//     const btns = makeElementWithClass({
-//       elementType: "div",
-//       className: "popup-btns",
-//     });
-//     const confirm = makeElementWithClass({
-//       elementType: "button",
-//       className: "popup-confirm",
-//       content: "확인",
-//     });
-//     const cancel = makeElementWithClass({
-//       elementType: "button",
-//       className: "popup-cancel",
-//       content: "취소",
-//     });
-
-//     btns.appendChild(confirm);
-//     btns.appendChild(cancel);
-//     popupBox.appendChild(message);
-//     popupBox.appendChild(btns);
-//     popup.appendChild(popupBox);
-//     this.$target.appendChild(popup);
-
-//     popup.addEventListener("click", (e) => {
-//       if (e.target === popup) {
-//         popup.style.display = "none";
-//         message.innerHTML = "";
-//       }
-//     });
-//   }
-// }
