386a1ca3eb708a26ea44c2288ee2a84f7b6439d4
Pigrabbit <donghyuk1003@gmail.com>
2020-07-22 13:44:17 +0900

    Update query in createCard funciton of CardRepository
    - DB 카드 테이블에 order_in_column이라는 필드 추가
    - 필드 추가에 따른 card 도메인 수정
    - DB Card 테이블에 insert 하기 위해 userid, columnid, order_in_column 필요
    - select 를 이용해 User, Columns 테이블로부터 정보를 가져옴
    - 이 과정을 transaction으로 묶음

--- server/domain/card.js
+++ server/domain/card.js
@@ -4,13 +4,15 @@ class Card {
     author,
     last_updated,
     content,
-    category
+    category,
+    order_in_column,
   }) {
     this.id = id;
     this.author = author;
     this.last_updated = last_updated;
     this.content = content;
     this.category = category;
+    this.order_in_column = order_in_column;
   }
 }
 
--- server/repository/card-repository.js
+++ server/repository/card-repository.js
@@ -6,9 +6,11 @@ class CardRepository {
 
   async findAllCards() {
     // TODO edit query
-    const [rows] = await this.db.query(
+    const conn = await this.db.getConnection();
+    const [rows] = await conn.query(
       "SELECT Cards.id, Users.username AS author,\
-       Cards.last_updated, Cards.content, column_name AS category\
+       Cards.last_updated, Cards.content, column_name AS category,\
+       Cards.order_in_column\
       FROM Cards \
       JOIN Users ON Cards.user_id = Users.id\
       JOIN Columns ON Cards.column_id = Columns.id");
@@ -16,34 +18,60 @@ class CardRepository {
     const cards = rows.map((row) => {
       return new this.cardDTO(row)
     });
+    await conn.release();
+
     return cards;
   }
 
   async findCardById(id) {
+    const conn = await this.db.getConnection();
     const query = "SELECT Cards.id, Users.username AS author,\
-      Cards.last_updated, Cards.content, column_name AS category\
+      Cards.last_updated, Cards.content, column_name AS category,\
+      Cards.order_in_column\
       FROM Cards \
       JOIN Users ON Cards.user_id = Users.id\
       JOIN Columns ON Cards.column_id = Columns.id WHERE Cards.id=? ";
 
-    const [rows] = await this.db.query(query, [id]);
+    const [rows] = await conn.query(query, [id]);
     const row = rows[0];
     const card = new this.cardDTO(row);
+    await conn.release();
 
     return card;
   }
 
+  // TODO: use transaction
   async createCard(cardDTO) {
-    const query =
-    `INSERT INTO todo.Cards (user_id, content, column_id)\
-    VALUES (\
-      (SELECT id FROM Users WHERE username="${cardDTO.author}")\
-      , ?, (SELECT id FROM Columns WHERE column_name="${cardDTO.category}"))`;
-
+    const conn = await this.db.getConnection();
     try {
-      await this.db.query(query, [cardDTO.content]);
-    } catch (err) {
-      throw err;
+      await conn.beginTransaction();
+
+      // userid 가져오기
+      const getUserIdQuery = "SELECT id FROM Users WHERE username=?";
+      [rows] = await conn.query(getUserIdQuery, [cardDTO.author]);
+      const userId = rows[0].id;
+
+      // column_id 가져오기
+      const getColumnIdQuery = "SELECT id FROM Columns WHERE column_name=?";
+      let [rows] = await conn.query(getColumnIdQuery, [cardDTO.category]);
+      const columnId = rows[0].id;
+
+      // column의 마지막 order 가져오기
+      const getLastOrderQeury = "SELECT order_in_column FROM Cards WHERE column_id=? ORDER BY order_in_column DESC LIMIT 1";
+      [rows] = await conn.query(getLastOrderQeury, [columnId]);
+      const lastOrderNumber = rows[0].order_in_column;
+
+      // 새로 card insert
+      const insertCardQuery = "INSERT INTO todo.Cards (user_id, content, column_id, order_in_column)\
+      VALUES (?, ?, ?, ?);"
+      await conn.query(insertCardQuery, [userId, cardDTO.content ,columnId, lastOrderNumber+1]);
+      await conn.commit();
+    } catch (error) {
+      console.error(error);
+      conn.rollback();
+      throw error;
+    } finally {
+      conn.release();
     }
   }
 
