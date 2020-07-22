94bf35bfdf5f2b9d6054546bc6c326aa8a4ecddd
Pigrabbit <donghyuk1003@gmail.com>
2020-07-22 12:27:37 +0900

    Add build script on project root directory
    - Client에서 웹팩빌드를 한 결과 파일을 Server의 public으로 복사해주는 스크립트 추가

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CLIENT_PUBLIC_BUNDLE_FILE=./client/public/index_bundle.js
+SERVER_PUBLIC_BUNDLE_FILE=./server/public/index_bundle.js
+
+if [ -f "$CLIENT_PUBLIC_BUNDLE_FILE" ]; then
+    echo "기존 client 번들 결과 파일을 삭제합니다..."
+    rm "$CLIENT_PUBLIC_BUNDLE_FILE"
+fi
+
+if [ -f "$SERVER_PUBLIC_BUNDLE_FILE" ]; then
+    echo "기존 server 번들 결과 파일을 삭제합니다..."
+    rm "$SERVER_PUBLIC_BUNDLE_FILE"
+fi
+
+cd ./client
+
+echo "FE 빌드를 시작합니다..."
+npx webpack
+
+echo "빌드 된 결과 bundle 파일을 server의 public 디렉토리로 복사 합니다..."
+cp ./public/index_bundle.js ../server/public/
+
+sleep 1
+
+if [ -f "$SERVER_PUBLIC_BUNDLE_FILE" ]; then
+    echo "$SERVER_PUBLIC_BUNDLE_FILE 이동 완료!\n"
+fi
