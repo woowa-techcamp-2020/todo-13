f0a18afc68ceb5435bb3f085e0be849406488b96
Pigrabbit <donghyuk1003@gmail.com>
2020-07-23 14:20:45 +0900

    Add getAllColumns, editColunName function in column controller

---
+++ server/controllers/column-controller.js
@@ -0,0 +1,11 @@
+async function getAllColumns(req, res, next) {
+  try {
+  } catch (error) {}
+}
+
+async function editColumnName(req, res, next) {
+  try {
+  } catch (error) {}
+}
+
+module.exports = {};
--- server/routes/column-router.js
+++ server/routes/column-router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const columnController = require("../controllers/column-controller");
 
-router.get("/", columnController.getAllColumnName);
+router.get("/", columnController.getAllColumns);
 router.put("/", columnController.editColumnName);
 
 module.exports = router;
